2eaab9d4935e635084d838e939dd9d7bcab39036
19nossila <163687585+19nossila@users.noreply.github.com>
2025-06-26 21:55:36 -0300

    Add files via upload

---
+++ Minecraft/src/net/minecraft/block/BlockButtonWood.java
@@ -0,0 +1,16 @@
+package net.minecraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockButton;
+import net.minecraft.util.Icon;
+
+public class BlockButtonWood extends BlockButton {
+
+   protected BlockButtonWood(int p_i9017_1_) {
+      super(p_i9017_1_, true);
+   }
+
+   public Icon func_71858_a(int p_71858_1_, int p_71858_2_) {
+      return Block.field_71988_x.func_71851_a(1);
+   }
+}
